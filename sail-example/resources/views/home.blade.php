a1cbeb8271e0e2ce4dd9ecf198fa13ece9e8e56e
ogstogst <ogst2266@gmail.com>
2024-02-26 21:59:02 +0900

    <update>homeビュー

--- sail-example/resources/views/home.blade.php
+++ sail-example/resources/views/home.blade.php
@@ -14,9 +14,40 @@
     </div>
     <div class="col-span-2 bg-white rounded p-4">
       <h2 class="text-2xl font-bold mb-2">新着レシピ</h2>
+      @foreach($recipes as $recipe)
+      <a href="" class="flex flex-col items-center bg-white mb-6 border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100">
+        <img src="{{ $recipe->image }}" class="object-cover rounded-t-lg h-40 w-40 mrounded-none rounded-l-lg" alt="{{ $recipe->title }}">
+        <div class="flex flex-col justify-between p-4 leading-normal">
+          <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-800">{{ $recipe->title }}</h5>
+          <p class="mb-3 font-normal">{{ $recipe->description }}</p>
+          <div class="flex">
+            <p class="font-bold mr-2">{{ $recipe->name }}</p>
+            <p class="text-gray-500">{{ $recipe->created_at->format('Y年m月d日') }}</p>
+          </div>
+        </div>
+      </a>
+      @endforeach
+      <a href="" class="block text-gray-600 font-bold text-right">すべてのレシピへ ></a>
     </div>
-    <div class="col-span-1 bg-slate-700 ml-4">
-      右
+    <div class="col-span-1 bg-gray ml-4">
+      <img src="/images/ad.png" alt="広告">
+    </div>
+  </div>
+  <div class="grid grid-cols-4">
+    <div class="col-span-3 bg-white rounded p-4">
+      <h2 class="text-2xl font-bold mb-2">人気レシピ</h2>
+      <div class="flex justify-between items-center mb-6">
+        @foreach($popular as $p)
+        <a href="" class="max-12 rounded overflow-hidden shadow-lg mx-4 hover:bg-gray-100">
+          <img src="{{ $p->image }}" alt="{{ $p->title }}" class="max-h-44 h-44 w-full object-cover">
+          <div class="px-6 py-4">
+            <div class="font-bold text-large mb-2">{{ $p->title }}</div>
+            <p class="text-gray-700 text-base">{{ $p->description }}</p>
+          </div>
+        </a>
+        @endforeach
+      </div>
+      <a href="" class="block text-gray-600 font-bold text-right">すべての人気レシピへ ></a>
     </div>
   </div>
 </x-app-layout>
